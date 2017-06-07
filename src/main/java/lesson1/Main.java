7a80e3d6a2f3c8b3f8ff0aa1ce0117b8b21a19df
Tony Kirin <entonyj@yandex.ru>
2017-06-07 19:25:05 +0500

    first commit

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+/**
+ * Created by entony on 07.06.17.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Don't know what im doing here=)");
+    }
+}
