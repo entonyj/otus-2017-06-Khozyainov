5336f0c3b15dc28a98a1768af58c2ebe77f3c4f4
Tony Kirin <entonyj@yandex.ru>
2017-07-29 18:22:54 +0300

    it seems it worked

--- lesson3/src/main/java/lesson3/Main.java
+++ lesson3/src/main/java/lesson3/Main.java
@@ -1,24 +1,41 @@
 package lesson3;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by entony on 12.07.17.
  */
 public class Main {
+
+    private static void printArray(List list, String name) {
+        int size = list.size();
+        System.out.println(name + ":");
+        for (int i = 0; i < size; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        MyArrayList<Integer> myArrayList = new MyArrayList<Integer>();
-        Integer[] srcArray = {1,2,3};
-        ArrayList<Integer> destArray = new ArrayList<Integer>(3);
+        List<Integer> array = new MyArrayList<>();
+        List<Integer> dest = new MyArrayList<>();
 
-        myArrayList.add(5);
-        for (Iterator<Integer> i = myArrayList.iterator(); i.hasNext();)
-            System.out.println(i.next());
-        System.out.println(myArrayList.size());
 
-         //Collections.addAll(myArrayList, srcArray);
-        //Collections.copy(destArray, myArrayList);
+        System.out.println("Checking Collections.addAll");
+        Collections.addAll(array, 1, 2, 3, 4, 5);
+        printArray(array, "array");
+        Collections.addAll(dest, 10, 11, 12, 13, 14);
+        printArray(dest, "dest");
+
+        System.out.println("\nChecking Collections.copy");
+        Collections.copy(dest, array);
+        printArray(dest, "dest");
+
+        System.out.println("\nChecking Collections.sort");
+        Collections.sort(dest, Collections.reverseOrder());
+        printArray(dest, "Reversed dest");
     }
 }
--- lesson3/src/main/java/lesson3/MyArrayList.java
+++ lesson3/src/main/java/lesson3/MyArrayList.java
@@ -1,83 +1,91 @@
 package lesson3;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
-
 import java.util.*;
-import java.util.function.UnaryOperator;
 
 /**
  * Created by entony on 13.07.17.
  */
-public class MyArrayList<T> implements List<T> {
+public class MyArrayList<T> implements List<T>, RandomAccess {
 
-    private Object[] myArrayList;
+    private Object[] data;
     private int size = 0;
     private int capacity = 10;
 
     private void checkCapacity(int minCapacity){
-        if (minCapacity - capacity > 0)
+        if (minCapacity > capacity)
             increaseCapacity();
     }
 
     private void increaseCapacity(){
         int newCapacity = (capacity * 3) / 2 + 1;
-        myArrayList = Arrays.copyOf(myArrayList, newCapacity);
+        data = Arrays.copyOf(data, newCapacity);
+    }
+
+    private void checkRange(int index){
+        if ((index > size)||(index < 0))
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds [0-" + size + "]");
+    }
+
+    private T data(int index){
+        return (T) data[index];
     }
 
     public MyArrayList() {
-        myArrayList = new Object[capacity];
+        data = new Object[capacity];
     }
 
-    public MyArrayList(int capacity) throws IllegalArgumentException {
+    public MyArrayList(int capacity) {
         if (capacity > 0) {
-            myArrayList = new Object[capacity];
+            data = new Object[capacity];
         } else if (capacity == 0) {
-            myArrayList = new Object[this.capacity];
+            data = new Object[this.capacity];
         } else {
             throw new IllegalArgumentException("Illegal size, must be > 0");
         }
     }
 
+    public MyArrayList(Collection<? extends T> c){
+        data = c.toArray();
+    }
+
     public boolean add(T e) {
         checkCapacity(size + 1);
-        myArrayList[size++] = e;
+        data[size++] = e;
         return true;
     }
 
     public Iterator<T> iterator() {
         return new Iterator<T>() {
 
-            private int currentIdx = 0;
+            private int currentIndex = 0;
 
             public boolean hasNext() {
-                return (currentIdx < size) && (myArrayList[currentIdx] != null);
+                return (currentIndex < size) && (data[currentIndex] != null);
             }
 
             public T next() {
-                if (currentIdx < size)
-                    return (T) myArrayList[currentIdx++];
+                if (currentIndex < size)
+                    return (T) data[currentIndex++];
                 return null;
             }
 
-            @Ignore
-            public void forEachRemaining() throws UnsupportedOperationException {
-            }
-
-            @Ignore
-            public void remove() throws UnsupportedOperationException {
-            }
         };
     }
 
-    //*TODO Realise methods: add, set, toArray,
-
-
-    public T set(int index, T element) {
-        return null;
+    public void sort(Comparator<? super T> c){
+        Arrays.sort((T[]) data, 0, size, c);
     }
 
-    public Object[] toArray() {
-        return new Object[0];
+    public T set(int index, T element) {
+        checkRange(index);
+        T prevElement = data(index);
+        data[index] = element;
+        return prevElement;
+    }
+
+    public T get(int index) {
+        checkRange(index);
+        return data(index);
     }
 
     public int size() {
@@ -88,12 +96,15 @@ public class MyArrayList<T> implements List<T> {
         return this.size == 0;
     }
 
+    public Object[] toArray() {
+        return Arrays.copyOf(data, size);
+    }
+
     public <T> T[] toArray(T[] a) {
         return null;
     }
 
-    public void sort(Comparator<? super T> c) {
-
+    public void clear() {
     }
 
     public ListIterator<T> listIterator() {
@@ -104,20 +115,16 @@ public class MyArrayList<T> implements List<T> {
         return null;
     }
 
-
-    //*TODO add exceptions
-
-
-    public boolean contains(Object o) {
+    public boolean removeAll(Collection<?> c) {
         return false;
     }
 
-    public boolean remove(Object o) {
+    public boolean retainAll(Collection<?> c) {
         return false;
     }
 
-    public boolean containsAll(Collection<?> c) {
-        return false;
+    public void add(int index, T element) {
+
     }
 
     public boolean addAll(Collection<? extends T> c) {
@@ -128,31 +135,15 @@ public class MyArrayList<T> implements List<T> {
         return false;
     }
 
-    public boolean removeAll(Collection<?> c) {
+    public boolean contains(Object o) {
         return false;
     }
 
-    public boolean retainAll(Collection<?> c) {
+    public boolean remove(Object o) {
         return false;
     }
 
-    public void replaceAll(UnaryOperator<T> operator) {
-
-    }
-
-    public void add(int index, T element) {
-
-    }
-
-    @Ignore
-    public void clear() throws UnsupportedOperationException {
-    }
-
-    public T get(int index) {
-        return null;
-    }
-
-    public T remove(int index) {
+    public List<T> subList(int fromIndex, int toIndex) {
         return null;
     }
 
@@ -160,16 +151,17 @@ public class MyArrayList<T> implements List<T> {
         return 0;
     }
 
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
     public int lastIndexOf(Object o) {
         return 0;
     }
 
-    public List<T> subList(int fromIndex, int toIndex) {
+    public T remove(int index) {
         return null;
     }
 
-    public Spliterator<T> spliterator() {
-        return null;
-    }
 
 }
