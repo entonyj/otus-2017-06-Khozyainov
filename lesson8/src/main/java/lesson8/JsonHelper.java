3bf0e3edff3ebbf21971495c833a93890bc58123
Tony Kirin <entonyj@yandex.ru>
2017-09-11 14:00:42 +0500

    realize collection to json

--- lesson8/src/main/java/lesson8/JsonHelper.java
+++ lesson8/src/main/java/lesson8/JsonHelper.java
@@ -24,10 +24,10 @@ public class JsonHelper {
         if (obj == null){
             return objBuilder.addNull(fieldName);
         } else if (cls.isPrimitive() || obj instanceof String){
-            return valueToJson(fieldName, obj, objBuilder);
+            valueToJson(fieldName, obj, objBuilder);
         } else if (cls.isArray()){
-            return objBuilder.add(fieldName, arrayToJson(obj, Json.createArrayBuilder()));
-        } else {
+            objBuilder.add(fieldName, arrayToJson(obj, Json.createArrayBuilder()));
+        } else if (obj instanceof Object){
             List<Field> fields = getFields(obj);
             for (Field field: fields){
 
@@ -42,37 +42,42 @@ public class JsonHelper {
                 Class type = field.getType();
                 fieldName = field.getName();
 
-                return objToJson(fieldName, value, objBuilder);
+                if (type.isPrimitive() || value instanceof String){
+                    valueToJson(fieldName, value, objBuilder);
+                } else if (type.isArray()){
+                    objBuilder.add(fieldName, arrayToJson(value, Json.createArrayBuilder()));
+                } else if (Collection.class.isAssignableFrom(type)){
+                    Object[] collection = ((Collection) value ).toArray();
+                    objBuilder.add(fieldName, arrayToJson(collection, Json.createArrayBuilder()));
+                } else
+                    objToJson(fieldName, value, objBuilder);
             }
         }
         return objBuilder;
     }
 
 
-    private static JsonObjectBuilder valueToJson (String fieldName, Object value, JsonObjectBuilder objBuilder) {
+    private static void valueToJson (String fieldName, Object value, JsonObjectBuilder objBuilder) {
         Class type = value.getClass();
-        if (type.isPrimitive()) {
-            if (value instanceof Byte) {
-                return objBuilder.add(fieldName, (byte) value);
-            } else if (value instanceof Integer) {
-                return objBuilder.add(fieldName, (int) value);
-            } else if (value instanceof Short) {
-                return objBuilder.add(fieldName, (short) value);
-            } else if (value instanceof Long) {
-                return objBuilder.add(fieldName, (long) value);
-            } else if (value instanceof Float) {
-                return objBuilder.add(fieldName, (float) value);
-            } else if (value instanceof Double) {
-                return objBuilder.add(fieldName, (double) value);
-            } else if (value instanceof Boolean) {
-                return objBuilder.add(fieldName, (boolean) value);
-            } else if (value instanceof Character) {
-                return objBuilder.add(fieldName, (char) value);
-            }
+        if (value instanceof Byte) {
+            objBuilder.add(fieldName, (byte) value);
+        } else if (value instanceof Integer) {
+            objBuilder.add(fieldName, (int) value);
+        } else if (value instanceof Short) {
+            objBuilder.add(fieldName, (short) value);
+        } else if (value instanceof Long) {
+            objBuilder.add(fieldName, (long) value);
+        } else if (value instanceof Float) {
+            objBuilder.add(fieldName, (float) value);
+        } else if (value instanceof Double) {
+            objBuilder.add(fieldName, (double) value);
+        } else if (value instanceof Boolean) {
+            objBuilder.add(fieldName, (boolean) value);
+        } else if (value instanceof Character) {
+            objBuilder.add(fieldName, (char) value);
         } else if (value instanceof String) {
-            return objBuilder.add(fieldName, (String) value);
+            objBuilder.add(fieldName, (String) value);
         }
-        return objBuilder;
     }
 
     private static JsonArrayBuilder arrayToJson (Object value, JsonArrayBuilder arrayBuilder){
@@ -98,7 +103,7 @@ public class JsonHelper {
             } else if (item instanceof String) {
                 arrayBuilder.add((String) item);
             } else if (item.getClass().isArray()){
-                arrayBuilder.add(arrayToJson(item, Json.createArrayBuilder()));
+                arrayToJson(item, arrayBuilder);
             } else if (Collection.class.isAssignableFrom(item.getClass())){
                 item = ((Collection) item).toArray();
                 arrayBuilder.add(arrayToJson(item, Json.createArrayBuilder()));
